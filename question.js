551ea43dd20699a4e5ea59d3d8c0ca71bb7b2659
CarylHenry <carolynhenry2021@u.northwestern.edu>
2019-02-21 01:15:35 -0600

    Consistent Styling

--- question.js
+++ question.js
@@ -1,28 +1,28 @@
-function setup() {
-  let buttons = document.getElementsByClassName('button')
-  for (var i = 0; i < buttons.length; i++) {
-    let buttonIndex = i
-    buttons[buttonIndex].onmousedown = function() {
-      buttonToggled(buttonIndex);
-    }
+function setup(){
+  var buttons = document.getElementsByClassName("button");
+  for(var i = 0; i<buttons.length; i++){
+    //light by default
+    buttons[i].style.backgroundColor="rgb(194, 210, 233)";
+    buttons[i].style.borderColor="#f1f1f0";
+    buttons[i].style.color="rgb(58, 78, 122)"
   }
 }
 
-function buttonToggled(index) {
-  console.log(index);
-
-  selectionElementId = 'selection-' + index.toString()
-
-  // console.log(selectionElementId);
-
-  selectionElement = document.getElementById(selectionElementId)
-  isSelected = selectionElement.className == "dot-selected"
-  if (isSelected) {
-    selectionElement.className = "dot"
-  } else {
-    selectionElement.className = "dot-selected"
+function toggle(button){
+  var color = button.style.backgroundColor;
+  //when the button is dark, make it light
+  if(color=="rgb(94, 131, 186)"){
+    button.style.background="rgb(194, 210, 233)";
+    button.style.color="rgb(58, 78, 122)";
+    button.style.borderWidth="0px";
+    button.style.padding="15px";
   }
-  console.log(isSelected);
+  //when the button is light, make it dark
+  else if(color=="rgb(194, 210, 233)"){
+    button.style.background="rgb(94, 131, 186)";
+    button.style.color="rgb(194, 210, 233)";
+    button.style.borderWidth="3px";
+    button.style.padding="12px";
 
-
+  }
 }
