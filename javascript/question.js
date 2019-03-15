ed052f346e075fe7ca925b8f36f10a3d2c8515a6
CarylHenry <carolynhenry2021@u.northwestern.edu>
2019-03-14 22:19:40 -0500

    Search errors for "l" and lawn mower attitude

--- javascript/question.js
+++ javascript/question.js
@@ -25,8 +25,8 @@ var jeansFirstQuestion = new Question("What characteristics do you care about?",
 var jeansSecondQuestion = new Question("How many of these things do you already own?", ["0", "1-5", "5-20", "My Last Name is Gates"], "I own...   ⏎");
 var jeansThirdQuestion = new Question("For what purpose do you wish to purchase a brand new pair of trousers of the denim or dungaree cloth variety?", ["Slothing around", "Sleeping", "Lakefill walks", "Looking like a boss"], "I want to use them for...   ⏎");
 
-var lawnFirstQuestion = new Question("How many hours it take for you to mow the lawn?", ["1-2", "3", "4-5", "+6"], "It takes...   ⏎")
-var lawnSecondQuestion = new Question("How frequently do you mow the lawn?", ["Once a Week", "Once Every 2 Weeks", "Once A Month", "I Barely Ever Mow It"], "I mow every...   ⏎")
+var lawnFirstQuestion = new Question("How many hours does it take for you to mow the lawn? Knowing you, I'm guessing a lifetime and a year.", ["1-2", "3", "4-5", "6+"], "It takes...   ⏎")
+var lawnSecondQuestion = new Question("I doubt that you even mow your lawn, but it's my job to ask the questions here. Riddle me this: how frequently do you mow the lawn?", ["Once a Week", "Once Every 2 Weeks", "Once A Month", "I Barely Ever Mow It"], "I mow every...   ⏎")
 var lawnThirdQuestion = new Question("Don't know why you'd enjoy mowing the lawn, but tell me how much you like that.", ["I Live For It", "I Look Forward to It", "I Don't Mind It", "I Hate It"], "I feel...   ⏎")
 
 // Initial Setup:
